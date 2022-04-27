62f9f5934aaec3ed5afea0f15f0837782344244b
JamesNg <njames15@gmail.com>
2022-04-27 17:02:45 -0400

    commit tree 77e768ce8ff896f77cc81e7922a66b42784107fb

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+//findMatching
+function findMatching(listArr, name) {
+    const result = listArr.filter(wanted => {
+        return wanted === name || wanted === name.toLowerCase(); 
+    });
+    return result;
+}   
+
+//fuzzyMatch
+function fuzzyMatch(listArr, name) {
+    const result = listArr.filter(first => first.startsWith(name));
+    return result;
+}
+
+//matchName
+function matchName(listArr, str) {
+    const result = listArr.filter(match => match.name === str)
+    return result;
+}
